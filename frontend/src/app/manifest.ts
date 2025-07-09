81b2f0723e4e87713f4cc80a2a1274f12c57637e
Matheus Henrique <84547045+Nerd-MHOV@users.noreply.github.com>
2025-07-09 09:58:48 -0300

    feat: init project and add pwa files

---
+++ frontend/src/app/manifest.ts
@@ -0,0 +1,20 @@
+import type { MetadataRoute } from 'next'
+
+export default function manifest(): MetadataRoute.Manifest {
+    return {
+        name: 'X-Pump',
+        short_name: 'X-Pump',
+        description: 'A gamification app for gyms with social media features and rankings.',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#ff8d46',
+        theme_color: '#011b2a',
+        icons: [
+            {
+                src: '/apple-icon.png',
+                sizes: '200x200',
+                type: 'image/svg',
+            },
+        ],
+    }
+}
