4117ddf0f79664106ecbc0f48c6757f8ee69da1c
Bruno Furlanetto <bruno.furlanetto@hotmail.com>
2025-07-09 19:07:57 -0300

    WorkoutCheckin model tests created and update model WorkoutCheckin after tests

--- backend/workouts/models.py
+++ backend/workouts/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
+from django.core.validators import FileExtensionValidator
 from django.db import models
 
 from status.models import Status
@@ -15,7 +16,18 @@ class WorkoutCheckin(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     check_in_type = models.CharField(max_length=100, choices=CheckinTypes.choices)
     image_proof = models.ImageField(upload_to='workout_checkins', null=True, blank=True)
-    video_proof = models.FileField(upload_to='workout_checkins', null=True, blank=True)
+    video_proof = models.FileField(
+        upload_to='workout_checkins',
+        null=True,
+        blank=True,
+        validators=[
+            FileExtensionValidator(
+                allowed_extensions=['mp4'],
+                message='Apenas arquivos de vídeo MP4 são permitidos.'
+            )
+        ],
+        help_text='Formatos suportados: MP4'
+    )
     location = models.CharField(max_length=100, null=True, blank=True)
     comments = models.TextField(blank=True)
     workout_date = models.DateTimeField()
@@ -25,7 +37,13 @@ class WorkoutCheckin(models.Model):
     def __str__(self):
         return f'Workout check-in for {self.user}'
 
+    def clean(self):
+        if self.check_in_type not in dict(CheckinTypes.choices):
+            raise ValueError(f'{self.check_in_type} is not a valid check-in type.')
+
     def save(self, *args, **kwargs):
+        self.clean()
+
         if self.check_in_type == 'PHOTO' and not self.image_proof:
             raise ValidationError('With check-in of type PHOTO, needed an image proof')
 
