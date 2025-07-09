6ce4ddf65143b03733cea3ef16bc1e8b5004e78e
Bruno Furlanetto <bruno.furlanetto@hotmail.com>
2025-07-09 17:54:41 -0300

    Model WorkoutCheckin created

--- backend/workouts/models.py
+++ backend/workouts/models.py
@@ -1,3 +1,25 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+from status.models import Status
+
+
+class CheckinTypes(models.TextChoices):
+    PHOTO = 'PHOTO', 'Foto'
+    VIDEO = 'VIDEO', 'Vídeo'
+    GEOLOCATION = 'GEOLOCATION', 'Geolocalização'
+
+
+class WorkoutCheckin(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    check_in_type = models.CharField(max_length=100, choices=CheckinTypes.choices)
+    image_proof = models.ImageField(upload_to='workout_checkins', null=True, blank=True)
+    video_proof = models.FileField(upload_to='workout_checkins', null=True, blank=True)
+    location = models.CharField(max_length=100, null=True, blank=True)
+    comments = models.TextField(blank=True)
+    workout_date = models.DateTimeField()
+    validation_status = models.ForeignKey(Status, on_delete=models.PROTECT)
+    base_points = models.FloatField(null=True, blank=True)
+
+    def __str__(self):
+        return f'Workout check-in for {self.user}'
