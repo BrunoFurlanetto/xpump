d621adf42f27da35080f45806e81b0a437081ef0
Bruno Furlanetto <bruno.furlanetto@hotmail.com>
2025-07-11 16:34:52 -0300

    feat: add API views for WorkoutCheckin with custom permissions

--- backend/workouts/views.py
+++ backend/workouts/views.py
@@ -1,3 +1,44 @@
 from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView, ListAPIView
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from rest_framework.response import Response
+from rest_framework import status, permissions
+from .models import WorkoutCheckin
+from .serializer import WorkoutCheckinSerializer
+
+
+class IsOwnerOrReadOnly(permissions.BasePermission):
+    """
+    Custom perms: Only the owner of the object can update or remove the post
+    Another users only can read
+    """
+    def has_object_permission(self, request, view, obj):
+        # Always allow safe requests (GET, HEAD, OPTIONS)
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        # To destructive or read methods, verify the object
+        # Have a field `user` equal to request.user
+        return obj.user == request.user
+
+
+class WorkoutCheckinsAPIView(ListCreateAPIView):
+    queryset = WorkoutCheckin.objects.all()
+    serializer_class = WorkoutCheckinSerializer
+    permission_classes = [IsAuthenticated]
+
+
+class WorkoutCheckinAPIView(RetrieveUpdateDestroyAPIView):
+    queryset = WorkoutCheckin.objects.all()
+    serializer_class = WorkoutCheckinSerializer
+    permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
+
+
+class WorkoutCheckinsByUserAPIView(ListAPIView):
+    serializer_class = WorkoutCheckinSerializer
+    permission_classes = [IsAuthenticated]
+    lookup_field = 'user_id'
+
+    def get_queryset(self):
+        return WorkoutCheckin.objects.filter(user=self.request.user).order_by('-workout_date')
