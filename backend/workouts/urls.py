8d348d38f3757784c4acbadd79c1535247314326
Bruno Furlanetto <bruno.furlanetto@hotmail.com>
2025-07-18 15:49:07 -0300

    WorkoutPlans endpoints created

--- backend/workouts/urls.py
+++ backend/workouts/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import WorkoutCheckinsAPIView, WorkoutCheckinAPIView, WorkoutCheckinsByUserAPIView
+from .views import WorkoutCheckinsAPIView, WorkoutCheckinAPIView, WorkoutCheckinsByUserAPIView, WorkoutPlanAPIView, \
+    WorkoutPlansAPIView
 
 urlpatterns = [
     path('', WorkoutCheckinsAPIView.as_view(), name='workout-list'),
     path('<int:pk>/', WorkoutCheckinAPIView.as_view(), name='workout-detail'),
     path('user/<int:user_id>/', WorkoutCheckinsByUserAPIView.as_view(), name='workout-by-user'),
+    path('plans/', WorkoutPlansAPIView.as_view(), name='workout-plans-list'),
+    path('plans/<int:pk>/', WorkoutPlanAPIView.as_view(), name='workout-plan-detail'),
 ]
