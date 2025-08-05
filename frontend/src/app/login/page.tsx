1b8677b5d0b33f4fe801f9838ceb85bad21d07e6
Matheus Henrique <84547045+Nerd-MHOV@users.noreply.github.com>
2025-08-05 19:20:29 -0300

    style: redoo login page with orange emphasis

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -4,8 +4,12 @@ import LoginForm from "./form-login";
 
 const Page = () => {
   return (
-    <div className="bg-background flex  w-full h-screen justify-center">
-      <div className="w-96 flex-col flex justify-center items-center">
+    <div className="bg-background flex w-full h-full justify-center">
+      <div className="w-full max-w-2xl h-[calc(100vh-200px)] bg-background relative">
+        <Image src="/lucasphelps.png" alt="Lucas Phelps" layout="fill" objectFit="cover" />
+      </div>
+
+      <div className="w-full flex-col h-screen flex justify-end pb-2 bottom-0 absolute items-center bg-gradient-to-t from-background via-background  to-background/10">
         <Image src={"/logo/logo.png"} alt="Logo" width={400} height={20} />
         <LoginForm />
       </div>
