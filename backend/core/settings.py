dfa97e7d696dfe8d56a0f85d404289171509b9da
Bruno Furlanetto <86568445+BrunoFurlanetto@users.noreply.github.com>
2025-08-11 18:17:04 -0300

    fix: onrender added to allowed hosts

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -42,7 +42,7 @@ if 'SECRET_KEY' in os.environ:
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'xpump.up.railway.app']
+ALLOWED_HOSTS = ['127.0.0.1', 'xpump.up.railway.app', 'xpump.onrender.com']
 
 # Application definition
 INSTALLED_APPS = [
