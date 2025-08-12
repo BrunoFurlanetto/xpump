d2a2bfd498d034890292cacfed734035cba39f58
Matheus Henrique <84547045+Nerd-MHOV@users.noreply.github.com>
2025-08-12 07:42:41 -0300

    fix: resolve build vercel

--- frontend/src/app/(app)/layout.tsx
+++ frontend/src/app/(app)/layout.tsx
@@ -1,40 +1,15 @@
-import { verifySession } from "@/lib/session";
-import { redirect } from "next/navigation";
-import { getUserById } from "../login/actions";
 import { UserAuthProvider } from "@/context/userAuthContext";
 import { NavigationProvider } from "@/context/navigationContext";
 import { AppNavigation } from "./_components/app-navigation";
 
-export default async function AppLayout({
+export default function AppLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  // Verificar sessão primeiro
-  const session = await verifySession(false);
-  if (!session) {
-    console.log("Sessão não encontrada no layout, redirecionando para login");
-    redirect("/login");
-  }
-
-  // Tentar buscar usuário
-  let user;
-  try {
-    user = await getUserById(session.user_id || "");
-  } catch (error) {
-    console.log("Erro ao buscar usuário no layout:", error);
-    user = null;
-  }
-
-  // Se não conseguir buscar o usuário, redirecionar para login
-  if (!user) {
-    console.log("Usuário não encontrado, redirecionando para login");
-    redirect("/login");
-  }
-
   return (
     <div className="bg-background min-h-screen">
-      <UserAuthProvider user={user}>
+      <UserAuthProvider>
         <NavigationProvider>
           <AppNavigation>{children}</AppNavigation>
         </NavigationProvider>
--- frontend/src/context/userAuthContext.tsx
+++ frontend/src/context/userAuthContext.tsx
@@ -1,5 +1,7 @@
 "use client";
-import { createContext, use } from "react";
+import { getUserById } from "@/app/login/actions";
+import { verifySession } from "@/lib/session";
+import { createContext, use, useEffect, useState } from "react";
 
 interface UserAuthContext {
   id: string;
@@ -11,13 +13,28 @@ interface UserAuthContext {
 
 export const UserAuthContext = createContext<UserAuthContext | undefined>(undefined);
 
-export function UserAuthProvider({
-  children,
-  user,
-}: {
-  readonly children: React.ReactNode;
-  readonly user: UserAuthContext;
-}) {
+export function UserAuthProvider({ children }: { readonly children: React.ReactNode }) {
+  const [user, setUser] = useState<UserAuthContext | undefined>(undefined);
+
+  useEffect(() => {
+    const getUser = async () => {
+      try {
+        const sessionUser = await verifySession();
+        if (!sessionUser) {
+          setUser(undefined);
+          return;
+        }
+        const aUser = await getUserById(sessionUser.user_id || "");
+        if (aUser) setUser(aUser);
+      } catch (error) {
+        console.error("Failed to verify session:", error);
+        setUser(undefined);
+      }
+    };
+
+    getUser();
+  }, []);
+
   return <UserAuthContext value={user}>{children}</UserAuthContext>;
 }
 
