1de58d661498b56b9f698280a07f5e092ef812cb
Bruno Furlanetto <86568445+BrunoFurlanetto@users.noreply.github.com>
2025-08-18 20:04:01 -0300

    feat: Enhance Meal model with streak tracking and validation

--- backend/nutrition/models.py
+++ backend/nutrition/models.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import User
 from django.core.validators import FileExtensionValidator
 from django.db import models
+from django.db.models.fields.related_descriptors.ForwardManyToOneDescriptor import RelatedObjectDoesNotExist
 
 from status.models import Status
 
@@ -33,7 +34,57 @@ class Meal(models.Model):
     multiplier = models.FloatField(default=1.0)
 
     def __str__(self):
-        return f"{self.user.username} - {self.meal_type.meal} at {self.meal_time.strftime('%Y-%m-%d %H:%M')}"
+        return f"{self.user.username} - {self.meal_type.meal_name} at {self.meal_time.strftime('%Y-%m-%d %H:%M')}"
+
+    def save(self, *args, **kwargs):
+        self.clean()
+        self.validation_status = Status.objects.filter(app_name='NUTRITION', action='PUBLISHED', is_active=True).first()
+
+        try:
+            # Update user's workout streak
+            self.user.meal_streak.update_streak(self.meal_time)
+        except RelatedObjectDoesNotExist:
+            # Create workout streak if it doesn't exist
+            MealStreak.objects.create(
+                user=self.user,
+                current_streak=1,
+                longest_streak=1,
+                last_workout_datetime=self.meal_time,
+            )
+
+        # Calculate multiplier and points based on streak
+        self.multiplier = self.update_multiplier()
+        self.base_points = float(10 * self.multiplier)
+
+        super().save(*args, **kwargs)
+
+        # Update the user's profile with the new points
+        self.user.profile.score += self.base_points
+        self.user.profile.save()
+
+    def clean(self):
+        if self.meal_type.interval_start < self.meal_time.time() < self.meal_type.interval_end:
+            raise ValueError("Meal time must be within the configured interval for this meal type.")
+
+        if Meal.objects.filter(user=self.user, meal_type=self.meal_type, meal_time__date=self.meal_time.date()).exists():
+            raise ValueError("A meal of this type has already been recorded for today.")
+
+    def update_multiplier(self):
+        streak = self.user.meal_streak.current_streak
+        meal_records = MealConfig.objects.count()
+
+        if streak < meal_records * 2:
+            self.multiplier = 1.0
+        elif meal_records * 2 <= streak < meal_records * 8:
+            self.multiplier = 1.25
+        elif meal_records * 8 <= streak < meal_records * 16:
+            self.multiplier = 1.50
+        elif meal_records * 16 <= streak < meal_records * 32:
+            self.multiplier = 1.75
+        elif streak >= meal_records * 32:
+            self.multiplier = 2.0
+
+        return self.multiplier
 
 
 class MealProof(models.Model):
@@ -80,3 +131,46 @@ class MealStreak(models.Model):
 
     def __str__(self):
         return f"Streak of {self.user.username}: {self.current_streak} (Máx: {self.longest_streak})"
+
+    def update_streak(self, meal_datetime):
+        if not self.last_meal_datetime:
+            self.current_streak = 1
+            self.longest_streak = 1
+        else:
+            if self.check_streak_ended(meal_datetime):
+                self.current_streak = 1
+            else:
+                self.current_streak += 1
+
+        if self.current_streak > self.longest_streak:
+            self.longest_streak = self.current_streak
+
+        self.last_meal_datetime = meal_datetime
+
+        return self.current_streak
+
+    def check_streak_ended(self, meal_datetime):
+        all_meals = MealConfig.objects.all().order_by('interval_end')
+        list_all_meals = list(all_meals)
+        current_meal_config = None
+        last_meal_config = None
+
+        for meal in all_meals:
+            if meal.interval_start <= meal_datetime.time() <= meal.interval_end:
+                current_meal_config = meal
+
+            if meal.interval_start <= self.last_meal_datetime.time() <= meal.interval_end:
+                last_meal_config = meal
+
+        if list_all_meals.index(current_meal_config) == 0 and list_all_meals.index(last_meal_config) == len(list_all_meals) - 1:
+            return False
+        else:
+            if list_all_meals.index(current_meal_config) == list_all_meals.index(last_meal_config) + 1:
+                return False
+            else:
+                return True
+
+    def check_and_reset_streak_if_ended(self, meal_datetime):
+        if self.check_streak_ended(meal_datetime):
+            self.current_streak = 0
+            self.save()
