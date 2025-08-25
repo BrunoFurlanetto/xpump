5dcf3f607f88009a3fd12b797cc2d60cd2adb96b
Bruno Furlanetto <86568445+BrunoFurlanetto@users.noreply.github.com>
2025-08-25 16:26:29 -0300

    chore: Reorganization of the group endpoint

--- backend/groups/urls.py
+++ backend/groups/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
-from .views import GroupsAPIView, GroupAPIView, GroupMembersAPIView, JoinGroupAPIView, GroupMemberAPIView
+from .views import GroupsAPIView, GroupAPIView, JoinGroupAPIView, GroupMemberAPIView
 
 urlpatterns = [
     path('', GroupsAPIView.as_view(), name='groups-list'),
     path('<int:pk>/', GroupAPIView.as_view(), name='groups-detail'),
-    path('<int:group_id>/members/', GroupMembersAPIView.as_view(), name='group-members-list'),
     path('<int:group_id>/members/<int:member_id>/', GroupMemberAPIView.as_view(), name='group-members-detail'),
     path('join/<str:invite_code>/', JoinGroupAPIView.as_view(), name='join-group'),
 ]
