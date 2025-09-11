222967dd258173b246c32ec642272f53861b1a49
Bruno Furlanetto <86568445+BrunoFurlanetto@users.noreply.github.com>
2025-09-11 18:14:06 -0300

    chore: Created new method to invite users to groups and added pending groups to user profile

---
+++ backend/groups/permissions.py
@@ -0,0 +1,64 @@
+from rest_framework.permissions import BasePermission
+from groups.models import GroupMembers
+
+
+class IsMember(BasePermission):
+    message = "User not a member of the group or membership is pending."
+
+    def _get_group_id(self, request, view):
+        return view.kwargs.get('group_id') or request.data.get('group_id')
+
+    def has_permission(self, request, view):
+        group_id = self._get_group_id(request, view)
+
+        if not group_id:
+            return False
+        try:
+            membership = GroupMembers.objects.get(group_id=group_id, member_id=request.user.id)
+        except GroupMembers.DoesNotExist:
+            return False
+
+        return membership.pending is False
+
+    def has_object_permission(self, request, view, obj):
+        group_id = self._get_group_id(request, view)
+
+        if not group_id:
+            return False
+        try:
+            membership = GroupMembers.objects.get(group_id=group_id, member_id=request.user.id)
+        except GroupMembers.DoesNotExist:
+            return False
+
+        return membership.pending is False
+
+
+class IsAdminMember(BasePermission):
+    message = "User is not an admin member of the group."
+
+    def _get_group_id(self, request, view):
+        return view.kwargs.get('group_id') or request.data.get('group_id')
+
+    def has_permission(self, request, view):
+        group_id = self._get_group_id(request, view)
+
+        if not group_id:
+            return False
+        try:
+            membership = GroupMembers.objects.get(group_id=group_id, member_id=request.user.id)
+        except GroupMembers.DoesNotExist:
+            return False
+
+        return membership.is_admin
+
+    def has_object_permission(self, request, view, obj):
+        group_id = self._get_group_id(request, view)
+
+        if not group_id:
+            return False
+        try:
+            membership = GroupMembers.objects.get(group_id=group_id, member_id=request.user.id)
+        except GroupMembers.DoesNotExist:
+            return False
+
+        return membership.is_admin and membership.pending is False
--- backend/groups/urls.py
+++ backend/groups/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import GroupsAPIView, GroupAPIView, JoinGroupAPIView, GroupMemberAPIView
+from .views import GroupsAPIView, GroupAPIView, InviteGroupAPIView, GroupMemberAPIView, InviteGroupAccept
 
 urlpatterns = [
     path('', GroupsAPIView.as_view(), name='groups-list'),
     path('<int:pk>/', GroupAPIView.as_view(), name='groups-detail'),
     path('<int:group_id>/members/<int:member_id>/', GroupMemberAPIView.as_view(), name='group-members-detail'),
-    path('join/<str:invite_code>/', JoinGroupAPIView.as_view(), name='join-group'),
+    path('<int:group_id>/invite/<str:user>/', InviteGroupAPIView.as_view(), name='invite-group'),
+    path('<int:group_id>/accept-invite/', InviteGroupAccept.as_view(), name='accept-invite-group'),
 ]
