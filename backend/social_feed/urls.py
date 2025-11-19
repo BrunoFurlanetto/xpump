c17fc32c3a7dc091e9345b54fcaf73feb9eb066a
Bruno Furlanetto <86568445+BrunoFurlanetto@users.noreply.github.com>
2025-11-19 17:37:58 -0300

    feat: Add initial setup for social feed app with routing and configuration

---
+++ backend/social_feed/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import (PostViewSet, CommentListCreateView, CommentDetailView, ReportListCreateView, ReportDetailView,
+                    UserPostsView)
+
+app_name = 'social_feed'
+
+router = DefaultRouter()
+router.register(r'posts', PostViewSet, basename='posts')
+
+urlpatterns = [
+    path('', include(router.urls)),
+    path('comments/', CommentListCreateView.as_view(), name='comment-list-create'),
+    path('comments/<int:pk>/', CommentDetailView.as_view(), name='comment-detail'),
+    path('reports/', ReportListCreateView.as_view(), name='report-list-create'),
+    path('reports/<int:pk>/', ReportDetailView.as_view(), name='report-detail'),
+    path('users/<int:user_id>/posts/', UserPostsView.as_view(), name='user-posts'),
+]
