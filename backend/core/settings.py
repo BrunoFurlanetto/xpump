7f547a408bf488f52ab52b52c851b4c6eee25ac7
Bruno Furlanetto <86568445+BrunoFurlanetto@users.noreply.github.com>
2025-12-03 16:01:09 -0300

    feat: Configure static file settings for Vercel deployment

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -127,6 +127,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'templates/static')
+]
+STATIC_ROOT = os.path.join('static')
+
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
