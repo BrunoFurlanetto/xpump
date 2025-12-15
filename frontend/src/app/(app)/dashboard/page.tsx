6f64f08ee8f887cb3a21dbe51bc26806a8bf3be5
nerd-mhov <matheus.henrique4245@gmail.com>
2025-12-15 18:16:53 -0300

    chore: isdmin

--- frontend/src/app/(app)/dashboard/page.tsx
+++ frontend/src/app/(app)/dashboard/page.tsx
@@ -43,7 +43,7 @@ export default function DashboardPage() {
   const isAdmin = roles.includes("admin") || roles.includes("Admin");
 
   // Se for admin, mostra o dashboard administrativo
-  if (!isAdmin) {
+  if (isAdmin) {
     return <AdminDashboardView />;
   }
 
